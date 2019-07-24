bf44f2bf4ffcacad045ec1ee34751ee5b160029c
Hongyu Wang <hongyuw2@illinois.edu>
2019-07-24 11:24:31 +0800

    upload Q215 with even better QuickSelect implementation

--- src/Q215_KthLargestElementInAnArray.java
+++ src/Q215_KthLargestElementInAnArray.java
@@ -3,51 +3,51 @@ import java.util.PriorityQueue;
 
 public class Q215_KthLargestElementInAnArray {
     static class Solution {
-//        private static void swap(int[] nums, int i, int j) {
-//            int temp = nums[i];
-//            nums[i] = nums[j];
-//            nums[j] = temp;
-//        }
-//
-//        private int partition(int[] nums, int lo, int hi) {
-//            int pivot = nums[lo];
-//            int i = lo, j = hi + 1;
-//            while (true) {
-//                while(i < hi && nums[++i] > pivot);
-//                while(j > lo && pivot > nums[--j]);
-//                if (i >= j) break;
-//                swap(nums, i, j);
-//            }
-//            swap(nums, lo, j);
-//            return j;
-//        }
-//
-//        public int findKthLargest(int[] nums, int k) {
-//            int lo = 0, hi = nums.length - 1;
-//            while (lo < hi) {
-//                int pivot = partition(nums, lo, hi);
-//                if(pivot < k - 1) lo = pivot + 1;
-//                else if (pivot > k - 1) hi = pivot - 1;
-//                else break;
-//            }
-//            return nums[k - 1];
-//        }
+        private static void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
 
-
-        public int findKthLargest(int[] nums, int k) {
-            PriorityQueue<Integer> heap = new PriorityQueue<>();
-            for(int num : nums) {
-                if(heap.size() < k) {
-                    heap.offer(num);
-                } else {
-                    if (num > heap.peek()) {
-                        heap.poll();
-                        heap.offer(num);
-                    }
+        private int quickSelect(int[] nums, int lo, int hi, int k) {
+            if (lo == hi) return nums[lo];
+            int left = lo, right = hi, mid = lo + (hi - lo) / 2;
+            int pivot = nums[mid];
+            while (left < right) {
+                while (left <= right && nums[left] > pivot) left++;
+                while (left <= right && nums[right] < pivot) right--;
+                if (left <= right) {
+                    swap(nums, left, right);
+                    left++;
+                    right--;
                 }
             }
-            return heap.peek();
+
+            int target = lo + k - 1;
+            if (target <= right) return quickSelect(nums, lo, right, k);
+            if (target >= left) return quickSelect(nums, left, hi,k - (left - lo));
+            return nums[right + 1];
         }
+
+        public int findKthLargest(int[] nums, int k) {
+            return quickSelect(nums,0, nums.length - 1, k);
+        }
+
+
+//        public int findKthLargest(int[] nums, int k) {
+//            PriorityQueue<Integer> heap = new PriorityQueue<>();
+//            for(int num : nums) {
+//                if(heap.size() < k) {
+//                    heap.offer(num);
+//                } else {
+//                    if (num > heap.peek()) {
+//                        heap.poll();
+//                        heap.offer(num);
+//                    }
+//                }
+//            }
+//            return heap.peek();
+//        }
     }
 
     public static void main(String[] args) {
