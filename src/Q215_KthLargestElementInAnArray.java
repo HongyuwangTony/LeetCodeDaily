4b69a8f66302dd033fa9409afac08477061cb11e
Hongyu Wang <hongyuw2@illinois.edu>
2019-07-24 11:11:07 +0800

    upload Q215 with PriorityQueue implementation

--- src/Q215_KthLargestElementInAnArray.java
+++ src/Q215_KthLargestElementInAnArray.java
@@ -1,35 +1,52 @@
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Q215_KthLargestElementInAnArray {
     static class Solution {
-        private static void swap(int[] nums, int i, int j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
+//        private static void swap(int[] nums, int i, int j) {
+//            int temp = nums[i];
+//            nums[i] = nums[j];
+//            nums[j] = temp;
+//        }
+//
+//        private int partition(int[] nums, int lo, int hi) {
+//            int pivot = nums[lo];
+//            int i = lo, j = hi + 1;
+//            while (true) {
+//                while(i < hi && nums[++i] > pivot);
+//                while(j > lo && pivot > nums[--j]);
+//                if (i >= j) break;
+//                swap(nums, i, j);
+//            }
+//            swap(nums, lo, j);
+//            return j;
+//        }
+//
+//        public int findKthLargest(int[] nums, int k) {
+//            int lo = 0, hi = nums.length - 1;
+//            while (lo < hi) {
+//                int pivot = partition(nums, lo, hi);
+//                if(pivot < k - 1) lo = pivot + 1;
+//                else if (pivot > k - 1) hi = pivot - 1;
+//                else break;
+//            }
+//            return nums[k - 1];
+//        }
 
-        private int partition(int[] nums, int lo, int hi) {
-            int pivot = nums[lo];
-            int i = lo, j = hi + 1;
-            while (true) {
-                while(i < hi && nums[++i] > pivot);
-                while(j > lo && pivot > nums[--j]);
-                if (i >= j) break;
-                swap(nums, i, j);
-            }
-            swap(nums, lo, j);
-            return j;
-        }
 
         public int findKthLargest(int[] nums, int k) {
-            int lo = 0, hi = nums.length - 1;
-            while (lo < hi) {
-                int pivot = partition(nums, lo, hi);
-                if(pivot < k - 1) lo = pivot + 1;
-                else if (pivot > k - 1) hi = pivot - 1;
-                else break;
+            PriorityQueue<Integer> heap = new PriorityQueue<>();
+            for(int num : nums) {
+                if(heap.size() < k) {
+                    heap.offer(num);
+                } else {
+                    if (num > heap.peek()) {
+                        heap.poll();
+                        heap.offer(num);
+                    }
+                }
             }
-            return nums[k - 1];
+            return heap.peek();
         }
     }
 
