2a6fbea189e68711a386b3d90f59c74a34801f98
Hongyu Wang <hongyuw2@illinois.edu>
2019-07-31 19:51:39 +0800

    upload Q692 skeleton

---
+++ src/Q692_TopKFrequentWords.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Q692_TopKFrequentWords {
+    static class Solution {
+        public List<String> topKFrequent(String[] words, int k) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().topKFrequent(new String[]{"i", "love", "leetcode", "i", "love", "coding"}, 2));
+        System.out.println(new Solution().topKFrequent(new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 4));
+    }
+}
