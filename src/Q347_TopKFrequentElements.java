25d640d479bea1c1a2bd941da5e1fd9d2d26d8ee
Hongyu Wang <hongyuw2@illinois.edu>
2019-08-01 17:29:30 +0800

    upload Q347 with HashMap+PriorityQueue

--- src/Q347_TopKFrequentElements.java
+++ src/Q347_TopKFrequentElements.java
@@ -1,9 +1,30 @@
-import java.util.List;
+import java.util.*;
 
 public class Q347_TopKFrequentElements {
     static class Solution {
         public List<Integer> topKFrequent(int[] nums, int k) {
-
+            HashMap<Integer, Integer> map_freq = new HashMap<>();
+            for (int num : nums) {
+                Integer freq = map_freq.get(num);
+                int newFreq = freq == null ? 1 : freq + 1;
+                map_freq.put(num, newFreq);
+            }
+            PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(new Comparator<Map.Entry<Integer, Integer>>() {
+                @Override
+                public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                    return o1.getValue() == o2.getValue() ?
+                            o2.getKey().compareTo(o1.getKey()) : o1.getValue().compareTo(o2.getValue());
+                }
+            });
+            int size = 0;
+            for (Map.Entry<Integer, Integer> entry : map_freq.entrySet()) {
+                pq.offer(entry);
+                if (size == k) pq.poll();
+                else size++;
+            }
+            LinkedList<Integer> list = new LinkedList<>();
+            while(!pq.isEmpty()) list.addFirst(pq.poll().getKey());
+            return list;
         }
     }
 
