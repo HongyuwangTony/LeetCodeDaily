778ce55995ce15cdc38cc7d8cbb959158b0f0c6a
Hongyu Wang <hongyuw2@illinois.edu>
2019-08-01 17:07:05 +0800

    upload Q347 skeleton

---
+++ src/Q347_TopKFrequentElements.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Q347_TopKFrequentElements {
+    static class Solution {
+        public List<Integer> topKFrequent(int[] nums, int k) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().topKFrequent(new int[]{1,1,1,2,2,3}, 2));
+        System.out.println(new Solution().topKFrequent(new int[]{1}, 1));
+    }
+}
