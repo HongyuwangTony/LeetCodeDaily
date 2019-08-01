21180b54a643a6339774b384fb01e9761ddeb4ef
Hongyu Wang <hongyuw2@illinois.edu>
2019-08-01 16:27:23 +0800

    upload Q139 as the practice today

---
+++ src/Q139_WordBreak.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Q139_WordBreak {
+    static class Solution {
+        public boolean wordBreak(String s, List<String> wordDict) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().wordBreak("leetcode", List.of("leet", "code")));
+        System.out.println(new Solution().wordBreak("applepenapple", List.of("apple", "pen")));
+        System.out.println(new Solution().wordBreak("catsandog", List.of("cats", "dog", "sand", "and", "cat")));
+    }
+}
