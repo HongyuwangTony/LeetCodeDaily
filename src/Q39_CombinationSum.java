7132aa3a28f436872e28470400fbe2f7cd12e13c
Hongyu Wang <hongyuw2@illinois.edu>
2019-08-02 20:36:07 +0800

    upload Q39 skeleton

---
+++ src/Q39_CombinationSum.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q39_CombinationSum {
+    static class Solution {
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().combinationSum(new int[]{2,3,6,7}, 7));
+        System.out.println(new Solution().combinationSum(new int[]{2,3,5}, 8));
+    }
+}
